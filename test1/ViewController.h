e224c53b49a0134ddd3c5cb41b8a652ea196a9c4
HongQuan <hq7781@hotmail.com>
2015-05-12 18:32:54 +0900

    Fix some problems

--- test1/ViewController.h
+++ test1/ViewController.h
@@ -8,8 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    IBOutlet UILabel *statusText;
+    IBOutlet UIButton *myButton;
 
+    IBOutlet UITextField *nameField;
+    IBOutlet UITextField *numberField;
+    
+}
+
+@property (retain, nonatomic) UILabel *statusText;
+@property (retain, nonatomic) UITextField *nameField;
+@property (retain, nonatomic) UITextField *numberFiled;
+
+// 左右键按下的时候响应方法
+- (IBAction)buttonPressed:(id)sender;
+// 编辑后按下Done后的响应方法
+- (IBAction) textFieldDoneEditing:(id)sender;
+// 数字键盘状态下，背景触摸即关闭数字键盘
+- (IBAction) backgroundClick:(id)sender;
+
+//- (IBAction) doSomething:(id)sender;
 
 @end
 
--- test1/ViewController.m
+++ test1/ViewController.m
@@ -14,6 +14,36 @@
 
 @implementation ViewController
 
+@synthesize statusText;
+@synthesize nameField;
+@synthesize numberFiled;
+
+
+
+#pragma mark - fucntions: 
+#pragma mark Button press action
+- (IBAction) buttonPressed: (id) sender{
+    NSString *title = [sender titleForState:(UIControlStateNormal)];
+    NSString *newText = [ [NSString alloc] initWithFormat:@" %@ button pressed.", title ];
+    statusText.text = newText;
+//    [newText release];
+}
+#pragma mark textField inputs action
+
+// 按下Done之后的处理
+-(IBAction) textFieldDoneEditing:(id)sender{
+    [sender resignFirstResponder];
+
+}
+
+//数字键盘状态的关闭处理
+-(IBAction)backgroundClick:(id)sender{
+    [nameField resignFirstResponder];
+    [numberFiled resignFirstResponder];
+    
+}
+
+#pragma mark - Lift cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,4 +54,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) dealloc {
+//    [statusText release];
+//    [super dealloc];
+}
+#pragma mark End
 @end
